b3f24386982dcf267dc726d2a3537d90a1d00079
Diamantine <meteoritus75@gmail.com>
2024-04-27 18:32:32 +0200

    Challenge les Classes

---
+++ Character.cs
@@ -0,0 +1,35 @@
+﻿namespace Les_classes
+{
+    public class Character
+    {
+        public string Name { get; set; }
+        public int PointsOfView { get; set; }
+        public int AttackForce { get; set; }
+        public int DefenseForce { get; set; }
+
+        public Character(int pointsOFView)
+        {
+            PointsOfView = pointsOFView;
+        }
+
+        public bool IsAlive()
+        {
+            Console.WriteLine(" Is view");
+            return true;
+        }
+        
+         public void Attack(Character otherCharacter) 
+         { 
+            PointsOfView = (PointsOfView) - (otherCharacter.AttackForce - DefenseForce);
+            if (PointsOfView <= 0 || otherCharacter.PointsOfView <= 0)
+            {
+                Console.WriteLine($"{Name} is died");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is view, pointsOFView number : {PointsOfView}");
+            }
+            
+         }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Les_classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character character1 = new Character(4);
+            character1.Name = "toto";
+            character1.AttackForce = 3;
+            character1.DefenseForce = 2;
+            
+            Character character2 = new Character(6);
+            character2.Name = "lulu";
+            character2.AttackForce = 5;
+            character2.DefenseForce = 3;
+
+            character1.Attack(character2);
+            character2.Attack(character1);
+            character1.Attack(character2);
+        }
+    }
+}
